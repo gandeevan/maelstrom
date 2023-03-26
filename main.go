420dc95e35b4fd6d63b051cb8dc47f6855fc33d7
Gandeevan Raghuraman <gandeevan@nianticlabs.com>
2023-03-25 17:25:23 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+// import "fmt"
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	n.Handle("echo", func(msg maelstrom.Message) error {
+		var body map[string]any
+		err := json.Unmarshal(msg.Body, &body)
+		if err != nil {
+			log.Printf("error unmarshaling echo message: %s", err)
+			return err
+		}
+		body["type"] = "echo_reply"
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
